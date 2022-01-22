85d96a96adbdd9d2b21b16674144926a16065da0
Isaac Ronan <isaacronan@users.noreply.github.com>
2022-01-22 09:31:04 -0500

    specified python path for jupyter

--- jupyter.sh
+++ jupyter.sh
@@ -1,2 +1,2 @@
 export CUR_DIR=`pwd`
-docker run -e ETRADE_KEY=$ETRADE_KEY -e ETRADE_SECRET=$ETRADE_SECRET -v $CUR_DIR:/usr/src/app --rm -it --publish 8088:8088 options-image:latest jupyter notebook --allow-root --ip 0.0.0.0 --port 8088 --notebook-dir notebooks
+docker run -e PYTHONPATH=/usr/src/app -e ETRADE_KEY=$ETRADE_KEY -e ETRADE_SECRET=$ETRADE_SECRET -v $CUR_DIR:/usr/src/app --rm -it --publish 8088:8088 options-image:latest jupyter notebook --allow-root --ip 0.0.0.0 --port 8088 --notebook-dir notebooks
