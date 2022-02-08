acfea39ecb4fa2d9352ec38a38e9264016cb2c32
Isaac Ronan <isaacronan@users.noreply.github.com>
2022-02-08 10:37:49 -0500

    remove unnecessary env variables from server container

--- server.sh
+++ server.sh
@@ -1,2 +1,2 @@
-docker container run --name options-server --network options-network -e PYTHONPATH=/options/src -e ETRADE_KEY=$ETRADE_KEY -e ETRADE_SECRET=$ETRADE_SECRET -v `pwd`:/options --rm -it --publish 8000:8000 --publish 8765:8765 options-image:latest \
+docker container run --name options-server --network options-network -e PYTHONPATH=/options/src -v `pwd`:/options --rm -it --publish 8000:8000 --publish 8765:8765 options-image:latest \
   /bin/bash -c 'python websocket_server.py > /dev/null 2>&1 & python -m http.server -d web > /dev/null 2>&1'
